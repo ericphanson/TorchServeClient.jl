8d4174ab8862cbf948fa7981f82bc3ef7025723a
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2024-03-31 00:28:27 +0100

    up

--- generator/run.sh
+++ generator/run.sh
@@ -1,5 +1,6 @@
 docker run --rm -v "${PWD}/..:/local" openapitools/openapi-generator-cli generate \
     -i /local/generator/pytorch-serve.json \
     -g julia-client \
+    -c /local/generator/config.json \
     -o /local \
     --skip-validate-spec
